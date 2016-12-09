2908cd7ebc959454fb24163e4bf01e7eb427e87d
Amol Dhage <amoltdhage@gmail.com>
2016-12-09 16:07:51 +0530

    Initial Commit

---
+++ Date Picker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Date Picker
+//
+//  Created by Amol Tukaram Dhage on 09/12/16.
+//  Copyright © 2016 AmolTDhage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Date Picker/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Date Picker
+//
+//  Created by Amol Tukaram Dhage on 09/12/16.
+//  Copyright © 2016 AmolTDhage. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
